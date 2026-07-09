ce8dd0a30249f2ea171306ba688d4db77e65676b
daanish04 <108368229+daanish04@users.noreply.github.com>
2026-07-10 03:38:13 +0530

    online tagged for players

--- rooms.js
+++ rooms.js
@@ -1,2 +1,9 @@
-// roomId: { id, topic, isPrivate (future: to show available public rooms), players: [ { id, name, score, status (LOBBY, INGAME) } ], status [(WAITING, RUNNING)], hostId }
+// roomId: { 
+// id
+// topic
+// isPrivate (future: to show available public rooms)
+// players: [ { id, name, score, status (LOBBY, INGAME), isOnline } ]
+// status [(WAITING, RUNNING)]
+// hostId
+// }
 export const rooms = new Map();
